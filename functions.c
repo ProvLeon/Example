cac43a13e006154b5264fec60b6caced52271d98
ProvLeon <providence.leonard@gmail.com>
2023-07-26 04:11:18 +0100

    commit

---
+++ functions.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int _add(int a, int b)
+{
+  return (a + b);
+}
+
+int _sub(int a, int b)
+{
+  return (a - b);
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _add(int a, int b);
+int _sub(int a, int b);
+
+
+#endif /* MAIN_H */
